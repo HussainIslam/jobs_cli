a7a966e8e85f82d96eba51954eb25c2550886a4d
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-11-22 23:17:09 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+from lxml import html
+import requests
+
+
+URL = 'https://ca.indeed.com/jobs?q=full+stack+developer&l=Scarborough%2C+ON'
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+results = soup.findAll('div', class_='jobsearch-SerpJobCard')
+for jobs in results:
+    job_title = jobs.find('h2', class_='title').a['title']
+    company_name = jobs.find('div', class_='sjcl').div.find('span', class_='company')
+    if company_name.a:
+        company_name = company_name.a.getText()
+    else:
+        company_name = company_name.getText()
+    print(f'{job_title}: company_name')
