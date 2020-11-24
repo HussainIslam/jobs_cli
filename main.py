5fc55b8f0b293bf737af1cb624223411e4e9bbc4
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-11-24 13:09:36 -0500

    added selenium, created browser object, parsed indeed job location

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from datetime import date
 from bs4 import BeautifulSoup
+from selenium import webdriver
 from lxml import html
 import requests
 
@@ -16,19 +18,32 @@ class JobPosting(object):
     def __str__(self):
         return f'{self.company} - {self.position}'
 
-    
+class Browser(object):
+    """
+    """
+    def __init__(self, webdriver_location):
+        self.webdriver_location = webdriver_location
+        options = webdriver.ChromeOptions()
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--incognito')
+        options.add_argument('--headless')
+        self.browser = webdriver.Chrome(webdriver_location, chrome_options=options)
 
 
 if __name__ == "__main__":
+    browser = Browser("/usr/lib/chromium-browser/chromedriver").browser
+
     URL = 'https://ca.indeed.com/jobs?q=full+stack+developer&l=Scarborough%2C+ON&fromage=1'
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    browser.get(URL)
+    page = browser.page_source
+    soup = BeautifulSoup(page, 'html.parser')
     results = soup.findAll('div', class_='jobsearch-SerpJobCard')
     counter = 0
-    for jobs in results:
-        job_header = jobs.find('h2', class_='title').a 
+    for job in results:
+        job_header = job.find('h2', class_='title').a 
         job_title = job_header['title']
         job_href = 'https://ca.indeed.com' +job_header['href']
+        job_date = date.today().strftime("%d-%m-%Y")
         headers={
             "referer": URL,
             'Access-Control-Allow-Origin': '*',
@@ -41,10 +56,18 @@ if __name__ == "__main__":
             posting = requests.get(job_href, headers=headers)
         except Exception as e:
             print(e)
-        company_name = jobs.find('div', class_='sjcl').div.find('span', class_='company')
+        company_name = job.find('div', class_='sjcl').div.find('span', class_='company')
         if company_name.a:
             company_name = company_name.a.getText()[1:]
         else:
             company_name = company_name.getText()[1:]
-        print(f'{counter}: {job_title} - {company_name}')
+        
+        job_location = job.find('div', class_='sjcl').select_one('.location').getText()
+        #if not job_location:
+        #    job_location = job.find('div', class_='sjcl').find('div', class_='location')
+        #job_location = job_location.getText()
+            
+        print(f'{counter}: {job_title} - {company_name} ({job_location})')
+        # if company_name == 'Procom':
+        #     print(job_location)
         counter+=1
