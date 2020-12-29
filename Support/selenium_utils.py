54e662281f6ff2dc477ef6f4d4f63fd433190ad0
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-12-29 13:36:19 -0500

    web scrapping is not allowed. so, archiving the project

--- Support/selenium_utils.py
+++ Support/selenium_utils.py
@@ -5,9 +5,12 @@ class Browser(object):
     """
     def __init__(self, webdriver_location):
         self.webdriver_location = webdriver_location
+        
         options = webdriver.ChromeOptions()
         options.add_argument('--ignore-certificate-errors')
         options.add_argument('--incognito')
         options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--remote-debugging-port=9222')
         self.browser = webdriver.Chrome(webdriver_location, chrome_options=options)
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from lxml import html
+import chromedriver_binary
 import requests
 
 
@@ -8,7 +9,7 @@ from Support.report_generate import ReportTextFile
 from Support.database_manager import DBM
 
 if __name__ == "__main__":
-    browser = Browser("/usr/lib/chromium-browser/chromedriver").browser
+    browser = Browser(chromedriver_binary.chromedriver_filename).browser
     new_jobs = list()
     dm = DataManager(browser, new_jobs)
     db = DBM()
